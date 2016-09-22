f355a7a83a02a6a23231bc3c9705a0b024ac14e0
Vincent <vincentnewkirk@gmail.com>
2016-09-21 16:39:18 -1000

    proof of concept delete method working

--- smart-yields/imports/ui/App.jsx
+++ smart-yields/imports/ui/App.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Posts } from '../api/posts.js';
+import ReactDOM from 'react-dom';
 
 import Post from './Post.jsx';
 
 class App extends React.Component{
   renderPosts(){
     return this.props.posts.map((posts) => (
-      <Post key={posts._id} post={posts} />
+      <Post key={posts._id} post={posts}/>
     ))
   };
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Posts.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
 
   render(){
     return (
       <div className='post-container'>
+        <form className="new-post" onSubmit={this.handleSubmit.bind(this)} >
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks"
+          />
+        </form>
         {this.renderPosts()}
       </div>
     )
@@ -22,6 +45,6 @@ class App extends React.Component{
 
 export default createContainer(() => {
   return {
-    posts: Posts.find({}).fetch(),
+    posts: Posts.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
--- smart-yields/imports/ui/Post.jsx
+++ smart-yields/imports/ui/Post.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Posts } from '../api/posts.js'
 
 class Post extends React.Component{
+  deleteThisPost(){
+    Posts.remove(this.props.post._id);
+  }
   render(){
     return(
       <div className='post-text'>
         {this.props.post.text}
+        <div className='delete' onClick={this.deleteThisPost.bind(this)}>Delete</div>
       </div>
     )
   }
 }
+
 
 export default Post
