7bae0bd57047b6e21185fadf9e835992e3ee92be
Vincent <vincentnewkirk@gmail.com>
2016-10-03 18:21:44 -1000

    new content form working, but adding to collection method not working yet

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -9,38 +9,29 @@ import { Meteor } from 'meteor/meteor';
 import { Button, Navbar, PageHeader, DropdownButton, MenuItem } from 'react-bootstrap';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
+import NewContentForm from './NewContentForm.jsx';
 import Post from './Post.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.inputChange = this.inputChange.bind(this);
     this.isValidInput = this.isValidInput.bind(this);
-    this.onSelectTemplate = this.onSelectTemplate.bind(this);
-    this.onSelectType =  this.onSelectType.bind(this);
-    this.onSelectMenu = this.onSelectMenu.bind(this);
-    this.state = {
-      validPath: true,
-      selectedTemplate: 1,
-      selectedType: 'Select Type',
-      menuSelected: false,
-      menuLocation: 'Menu Location',
-    }
   }
 
-  handleSubmit(event) {
+  handleSubmit(event, title, path, text) {
+    console.log(event);
     event.preventDefault();
 
     // Find the text field via the React ref
-    const text = this.refs.textInput.value.trim();
-    const title = this.refs.titleInput.value.trim();
-    const path = '/' + this.refs.pathInput.value.trim();
-    const template = this.state.selectedTemplate;
+    // const text = this.refs.textInput.value.trim();
+    // const title = this.refs.titleInput.value.trim();
+    // const path = '/' + this.refs.pathInput.value.trim();
+    // const template = this.state.selectedTemplate;
 
     //validate URL input doesn't contain special characters
     //or is already a path in use
-    if(this.isValidInput(this.refs.pathInput.value.trim())){
+    if(this.isValidInput(path)){
       this.props.posts.forEach((post) => {
         if(post.path === path){
           this.setState({ validPath: false });
@@ -54,9 +45,9 @@ class App extends React.Component {
     }
 
     // Clear form
-    this.refs.textInput.value = '';
-    this.refs.titleInput.value = '';
-    this.refs.pathInput.value = '';
+    // this.refs.textInput.value = '';
+    // this.refs.titleInput.value = '';
+    // this.refs.pathInput.value = '';
   }
 
   isValidInput(str) {
@@ -70,75 +61,19 @@ class App extends React.Component {
     }
   }
 
-  inputChange() {
-    this.setState({ validPath: true })
-  }
-
-  onSelectTemplate(event) {
-    this.setState({ selectedTemplate: event })
-  }
-
-  onSelectType(event) {
-    this.setState({ selectedType: event });
-    if(event === 'Page'){
-      this.setState({ menuSelected: true });
-    } else {
-      this.setState({ menuSelected: false });
-    }
-  }
-
-  onSelectMenu(event){
-    this.setState({ menuLocation: event })
-  }
-
   render() {
+    console.log(this.props, 'App props')
     return (
       <div className="post-container container">
 
         <AccountsUIWrapper />
         <PageHeader>Smart Yields Custom CMS</PageHeader>
-        { this.props.currentUser ?
-          <Navbar>
-            <form className="new-post" onSubmit={this.handleSubmit} >
-              <br />
-              <span>Title of your page</span><input
-                type="text"
-                ref="titleInput"
-                placeholder="Awesome Page"
-              /><br />
-              <span>smartyields.com/</span><input
-                type="text"
-                ref="pathInput"
-                placeholder="desired URL path"
-                onChange={this.inputChange}
-              />{this.state.validPath ?
-                  null
-                  : <span>Invalid URL. Specified URL may already be in use</span>
-                }<br />
-              <span>Body of your page</span><input
-                type="text"
-                ref="textInput"
-                placeholder="'Hello! This is my page!'"
-              /><br />
-              <DropdownButton title={'Template ' + this.state.selectedTemplate} onSelect={this.onSelectTemplate} id="1337">
-                <MenuItem eventKey={1} ref="template1">Template 1</MenuItem>
-                <MenuItem eventKey={2} ref="template2">Template 2</MenuItem>
-              </DropdownButton>
-            </form>
-            <DropdownButton title={this.state.selectedType} onSelect={this.onSelectType} id="137">
-              <MenuItem eventKey={'Post'} ref="template1">Post</MenuItem>
-              <MenuItem eventKey={'Page'} ref="template2">Page</MenuItem>
-            </DropdownButton>
-            {this.state.menuSelected
-             ?<DropdownButton title={this.state.menuLocation} onSelect={this.onSelectMenu} id="17">
-                <MenuItem eventKey={'Main'}>Main</MenuItem>
-                <MenuItem eventKey={'Sidebar'}>Sidebar</MenuItem>
-                <MenuItem eventKey={'Footer'}>Footer</MenuItem>
-              </DropdownButton>
-              : null
-            }
-            <Button onClick={this.handleSubmit} bsStyle="primary">Save</Button>
-          </Navbar>
+        {
+          this.props.currentUser
+          ? <NewContentForm
+            handleSubmit={this.handleSubmit}
+            isValidInput={this.isValidInput}
+            />
           : null
         }
         {this.props.yield}
--- imports/ui/NewContentForm.jsx
+++ imports/ui/NewContentForm.jsx
@@ -7,54 +7,85 @@ import { createContainer } from 'meteor/react-meteor-data';
 class NewContentForm extends React.Component {
   constructor(){
     super();
+    this.onSelectTemplate = this.onSelectTemplate.bind(this);
+    this.onSelectType =  this.onSelectType.bind(this);
+    this.onSelectMenu = this.onSelectMenu.bind(this);
+    this.inputChange = this.inputChange.bind(this);
 
+    this.state = {
+      validPath: true,
+      selectedTemplate: 1,
+      selectedType: 'Select Type',
+      menuSelected: false,
+      menuLocation: 'Menu Location',
+    }
   }
+
+  inputChange() {
+    this.setState({ validPath: true })
+  }
+
+  onSelectTemplate(event) {
+    this.setState({ selectedTemplate: event })
+  }
+
+  onSelectType(event) {
+    this.setState({ selectedType: event });
+    if(event === 'Page'){
+      this.setState({ menuSelected: true });
+    } else {
+      this.setState({ menuSelected: false });
+    }
+  }
+
+  onSelectMenu(event){
+    this.setState({ menuLocation: event })
+  }
+
   render() {
+    console.log(this.props);
     return (
-      { this.props.currentUser ?
-        <Navbar>
-          <form className="new-post" onSubmit={this.handleSubmit} >
-            <br />
-            <span>Title of your page</span><input
-              type="text"
-              ref="titleInput"
-              placeholder="Awesome Page"
-            /><br />
-            <span>smartyields.com/</span><input
-              type="text"
-              ref="pathInput"
-              placeholder="desired URL path"
-              onChange={this.inputChange}
-            />{this.state.validPath ?
-                null
-                : <span>Invalid URL. Specified URL may already be in use</span>
-              }<br />
-            <span>Body of your page</span><input
-              type="text"
-              ref="textInput"
-              placeholder="'Hello! This is my page!'"
-            /><br />
-            <DropdownButton title={'Template ' + this.state.selectedTemplate} onSelect={this.onSelectTemplate} id="1337">
-              <MenuItem eventKey={1} ref="template1">Template 1</MenuItem>
-              <MenuItem eventKey={2} ref="template2">Template 2</MenuItem>
-            </DropdownButton>
-          </form>
-          <DropdownButton title={this.state.selectedType} onSelect={this.onSelectType} id="137">
-            <MenuItem eventKey={'Post'} ref="template1">Post</MenuItem>
-            <MenuItem eventKey={'Page'} ref="template2">Page</MenuItem>
+      <Navbar>
+        <form className="new-post" onSubmit={this.props.handleSubmit} >
+          <br />
+          <span>Title of your page</span><input
+            type="text"
+            ref="titleInput"
+            placeholder="Awesome Page"
+          /><br />
+          <span>smartyields.com/</span><input
+            type="text"
+            ref="pathInput"
+            placeholder="desired URL path"
+            onChange={this.inputChange}
+          />{this.state.validPath ?
+              null
+              : <span>Invalid URL. Specified URL may already be in use</span>
+            }<br />
+          <span>Body of your page</span><input
+            type="text"
+            ref="textInput"
+            placeholder="'Hello! This is my page!'"
+          /><br />
+          <DropdownButton title={'Template ' + this.state.selectedTemplate} onSelect={this.onSelectTemplate} id="1337">
+            <MenuItem eventKey={1} ref="template1">Template 1</MenuItem>
+            <MenuItem eventKey={2} ref="template2">Template 2</MenuItem>
           </DropdownButton>
-          {this.state.menuSelected
-           ?<DropdownButton title={this.state.menuLocation} onSelect={this.onSelectMenu} id="17">
-              <MenuItem eventKey={'Main'}>Main</MenuItem>
-              <MenuItem eventKey={'Sidebar'}>Sidebar</MenuItem>
-              <MenuItem eventKey={'Footer'}>Footer</MenuItem>
-            </DropdownButton>
-            : null
-          }
-          <Button onClick={this.handleSubmit} bsStyle="primary">Save</Button>
-        </Navbar>
-        : null
-      }
+        </form>
+        <DropdownButton title={this.state.selectedType} onSelect={this.onSelectType} id="137">
+          <MenuItem eventKey={'Post'} ref="template1">Post</MenuItem>
+          <MenuItem eventKey={'Page'} ref="template2">Page</MenuItem>
+        </DropdownButton>
+        {this.state.menuSelected
+         ?<DropdownButton title={this.state.menuLocation} onSelect={this.onSelectMenu} id="17">
+            <MenuItem eventKey={'Main'}>Main</MenuItem>
+            <MenuItem eventKey={'Sidebar'}>Sidebar</MenuItem>
+            <MenuItem eventKey={'Footer'}>Footer</MenuItem>
+          </DropdownButton>
+          : null
+        }
+        <Button onClick={this.props.handleSubmit} bsStyle="primary">Save</Button>
+      </Navbar>
     )
   }
 }
