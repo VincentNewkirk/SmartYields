be56f41ab4f708a7c18ab84f4fdd56a3cf420627
Vincent <vincentnewkirk@gmail.com>
2016-10-05 17:10:46 -1000

    wrote primitive algorithm to  push posts into an array limiting 5 at a time

--- imports/ui/PostTags.jsx
+++ imports/ui/PostTags.jsx
@@ -33,7 +33,7 @@ class PostTags extends React.Component {
         }
         <Pagination
           bsSize="medium"
-          items={10}
+          items={5}
           activePage={this.state.activePage}
           onSelect={this.handleSelect} />
       </div>
@@ -43,8 +43,25 @@ class PostTags extends React.Component {
 
 export default createContainer(() => {
   Meteor.subscribe('posts');
+  const initalPosts = Posts.find({}, { sort: { createdAt: -1 } }).fetch();
+  let array = [];
+  let arrToPush = [];
+  let counter = 0;
+    for(let i = 0; i < initalPosts.length; i++){
+      if(counter === 5){
+        array.push(arrToPush)
+        arrToPush = [];
+        counter = 0;
+        }
+      arrToPush.push(initalPosts[i]);
+      counter += 1;
+    }
+  if(arrToPush.length){
+    array.push(arrToPush)
+  }
 
   return {
+    sortedPosts: array,
     posts: Posts.find({}, { sort: { createdAt: -1 } }).fetch(),
     currentUser: Meteor.user(),
   };
