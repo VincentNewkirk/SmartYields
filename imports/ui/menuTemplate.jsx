4e73fd17e1fc0e9c42605b700a4eaef95c3ca74e
Vincent <vincentnewkirk@gmail.com>
2016-10-08 18:33:07 -1000

    create menuTemplate.jsx

---
+++ imports/ui/menuTemplate.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuComponent from './menuComponent.jsx';
+
+
+class MenuTemplate extends React.Component {
+  render() {
+    return(
+      <div>
+        <MenuComponent />
+      </div>
+    )
+  }
+}
