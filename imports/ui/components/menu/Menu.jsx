7edc47a5a8c2f601afb024cc4c533390c8fc5448
Justin Hedani <jkhedani@gmail.com>
2016-10-13 15:51:59 -1000

    Create scaffolding for new menu structure

---
+++ imports/ui/components/menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Pages } from '/imports/api/pages.js';
+
+class Menu extends React.Component {
+
+};
+
+export default createContainer(() => {
+
+});
