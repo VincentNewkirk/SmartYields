8e3dbf2594fa18b3b0a8e683751a9b94862c8c19
Justin Hedani <jkhedani@gmail.com>
2016-10-13 15:02:00 -1000

    Add posts manually to primary menu

--- imports/ui/layout/Header.jsx
+++ imports/ui/layout/Header.jsx
@@ -10,22 +10,22 @@ class Header extends React.Component {
             <Navbar inverse>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#">Smart Yields</a>
+                        <a href="/">Smart Yields</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
 
-                        <NavItem eventKey={1} href="#">LInk</NavItem>
-                        <NavItem eventKey={2} href="#">Link</NavItem>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                        <NavItem eventKey={1} href="/posts">Posts</NavItem>
+                        {/* <NavItem eventKey={2} href="#">Link</NavItem> */}
+                        {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
                             <MenuItem eventKey={3.1}>Action</MenuItem>
                             <MenuItem eventKey={3.2}>Another action</MenuItem>
                             <MenuItem eventKey={3.3}>Something else here</MenuItem>
                             <MenuItem divider />
                             <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                     <Nav pullRight>
                         <NavItem eventKey={1} href="#"><AccountsUIWrapper /></NavItem>
