fe52712d309865d884f942496a123a4d0a316c8d
Justin Hedani <jkhedani@gmail.com>
2016-10-17 15:25:24 -1000

    Formatting

--- imports/ui/layout/Header.jsx
+++ imports/ui/layout/Header.jsx
@@ -17,24 +17,7 @@ class Header extends Component {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-
           <Menu location="Main" />
-          {/* <Nav>
-            <NavItem eventKey={1} href="/posts">Posts</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav> */}
-          {/* <ul className="nav navbar-nav">
-            <li><a href="#">Test</a></li>
-            <li><a href="#">Testing</a></li>
-          </ul> */}
-
           <Nav pullRight>
             <NavItem eventKey={1} href="#"><AccountsUIWrapper /></NavItem>
             {/* <NavItem eventKey={2} href="#">Link Right</NavItem> */}
