eb12f85f529b6bdd9ad9509aedd0a83e0d406a66
Vincent <vincentnewkirk@gmail.com>
2016-10-21 19:22:47 -1000

    linted routing.jsx

--- lib/routing.jsx
+++ lib/routing.jsx
@@ -1,78 +1,76 @@
 import React from 'react';
-import {mount} from 'react-mounter';
+import { mount } from 'react-mounter';
 import App from '/imports/ui/App.jsx';
-import PostTags from '/imports/ui/components/post/PostTags.jsx';
 import TemplateSelector from '/imports/ui/templateSelector.jsx';
 import PostTagRenderer from '/imports/ui/components/post/PostTagRenderer.jsx';
-import MenuTemplate from '/imports/ui/components/menu.old/menuTemplate.jsx';
 import MenuComponent from '/imports/ui/components/menu.old/menuComponent.jsx';
 
 FlowRouter.route('/', {
   name: 'landing page',
-  subscriptions: function(){
+  subscriptions: function () {
     this.register('files', Meteor.subscribe('files'));
   },
-  action: function(params, queryParams){
-    mount( App );
-  }
-})
+  action: function () {
+    mount(App);
+  },
+});
 
 FlowRouter.route('/posts/', {
   name: 'posts',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('posts', Meteor.subscribe('posts'));
   },
-  action(params){
-    mount( App, {yield: <PostTagRenderer />})
-  }
-})
+  action() {
+    mount(App, { yield: <PostTagRenderer /> });
+  },
+});
 
 FlowRouter.route('/posts/:id', {
   name: 'posts',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('posts', Meteor.subscribe('posts'));
   },
-  action(params){
-    mount( App, {yield: <TemplateSelector pathLink={params.id} type="post"/>})
-  }
-})
+  action(params) {
+    mount(App, { yield: <TemplateSelector pathLink={params.id} type="post"/> });
+  },
+});
 
 FlowRouter.route('/:id', {
   name: 'page-edit',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('pages', Meteor.subscribe('pages'));
   },
-  action(params){
-    mount( App, {yield: <TemplateSelector pathLink={params.id} type="page"/>})
-  }
-})
+  action(params) {
+    mount(App, { yield: <TemplateSelector pathLink={params.id} type="page"/> });
+  },
+});
 
 FlowRouter.route('/pages-primary/', {
   name: 'pages-primary',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('pages', Meteor.subscribe('pages'));
   },
-  action(params){
-    mount( App, {yield: <MenuComponent location="primary" />})
-  }
-})
+  action() {
+    mount(App, { yield: <MenuComponent location="primary" /> });
+  },
+});
 
 FlowRouter.route('/pages-sidebar/', {
   name: 'pages-sidebar',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('pages', Meteor.subscribe('pages'));
   },
-  action(params){
-    mount( App, {yield: <MenuComponent location="sidebar" />})
-  }
-})
+  action() {
+    mount(App, { yield: <MenuComponent location="sidebar" /> });
+  },
+});
 
 FlowRouter.route('/pages-footer/', {
   name: 'pages-footer',
-  subscriptions: function(params) {
+  subscriptions: function () {
     this.register('pages', Meteor.subscribe('pages'));
   },
-  action(params){
-    mount( App, {yield: <MenuComponent location="footer" />})
-  }
-})
+  action() {
+    mount(App, { yield: <MenuComponent location="footer" /> });
+  },
+});
